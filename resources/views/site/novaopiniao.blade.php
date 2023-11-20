0689a4daf752b9be80658a7346009e6e885170a7
JoaoAbram <131903381+JoaoAbram@users.noreply.github.com>
2023-11-20 14:03:59 -0300

    novaopiniao

--- resources/views/site/novaopiniao.blade.php
+++ resources/views/site/novaopiniao.blade.php
@@ -1,4 +1,4 @@
-<h1>Dê sua opinião</h1>
+<h1>FÁBRICA DE SORRISOS</h1>
 <form action="{{route('novaopiniao.index')}}" method="POST">
     @csrf()
 
@@ -13,3 +13,50 @@
     <textarea name="justifique" rows="5" cols="33" placeholder="justifique"></textarea>
     <button type="submit">Enviar</button>
 </form>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: #2B1F31;
+        font-family: Arial, sans-serif;
+    }
+
+    h1 {
+        background: #6341B4;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 12vh;
+        font-family: 'Fredoka One', cursive;
+        color: white;
+        margin-bottom: 20px;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    select, textarea {
+        margin: 10px 0;
+        padding: 10px;
+        width: 300px;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        background-color: white; /* Cor de fundo das caixas de entrada */
+    }
+
+    button {
+        background-color: #C4FF10;
+        color: black;
+        padding: 10px 15px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
